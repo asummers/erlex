f4add4ff9ec74ec143ff1091200eab2acc669bbc
Andrew Summers <oasummerso@gmail.com>
2018-09-19 07:51:32 -0500

    Reignore warning.

--- dialyzer.ignore_warnings.exs
+++ dialyzer.ignore_warnings.exs
@@ -3,5 +3,6 @@
   {"lib/erlex.ex", :no_return, 130},
   {"lib/erlex.ex", :no_return, 140},
   {"lib/erlex.ex", :no_return, 159},
+  {"lib/erlex.ex", :no_return, 180},
   {"lib/erlex.ex", :no_return, 536},
 ]
