b2aba265ae730286e47b81afd4eb5134041d762a
Andrew Summers <oasummerso@gmail.com>
2019-07-18 19:25:24 -0500

    Format

--- test/pretty_print_test.exs
+++ test/pretty_print_test.exs
@@ -341,8 +341,7 @@ defmodule Erlex.Test.PretyPrintTest do
 
     pretty_printed = Erlex.pretty_print(input)
 
-    expected_output =
-      "(Atom, Encoding) :: binary() when Atom: atom(), Encoding: :latin1 | :utf8"
+    expected_output = "(Atom, Encoding) :: binary() when Atom: atom(), Encoding: :latin1 | :utf8"
 
     assert pretty_printed == expected_output
   end
